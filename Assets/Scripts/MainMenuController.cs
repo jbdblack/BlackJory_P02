c5a026e1fd3a51cf692acf72cf44bd29cd0591e6
Jory <jbb140030@utdallas.edu>
2021-03-07 15:05:57 -0600

    Added AudioManager

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+
+    void Start()
+    {
+        if(_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+}
