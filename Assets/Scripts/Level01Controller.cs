5b186a89ac53d57813db05e0d243acbf49fc5bdf
Jory <jbb140030@utdallas.edu>
2021-03-07 15:25:50 -0600

    Added score tracker

--- Assets/Scripts/Level01Controller.cs
+++ Assets/Scripts/Level01Controller.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Level01Controller : MonoBehaviour
 {
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+
     private void Update()
     {
+        // Increase Score
+        //TODO replace with real implementation later
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+        // Exit Level
+        //TODO bring up popup menu for navigation
+
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ExitLevel();
@@ -15,5 +30,14 @@ public class Level01Controller : MonoBehaviour
     public void ExitLevel()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        // increase score
+        _currentScore += scoreIncrease;
+        // update score display, so we can see the new score
+        _currentScoreTextView.text =
+            "Score: " + _currentScore.ToString();
     }
 }
