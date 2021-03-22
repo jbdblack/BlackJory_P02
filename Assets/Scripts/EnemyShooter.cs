c88d4caac0c749b12d7e07f1bf0217bf5a1dd946
Jory <jbb140030@utdallas.edu>
2021-03-21 21:56:45 -0500

    Project02C
    Added raycast to weapon, created enemy and added behavior to follow the player when in range

---
+++ Assets/Scripts/EnemyShooter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyShooter : MonoBehaviour
+{
+    public int _enemyHealth = 100;
+    public LayerMask aggroLayerMask;
+
+    private NavMeshAgent navAgent;
+    private Collider[] withinAggroColliders;
+    private FirstPersonPlayer player;
+
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate()
+    {
+        withinAggroColliders = Physics.OverlapSphere(transform.position, 10, aggroLayerMask);
+        if(withinAggroColliders.Length > 0)
+        {
+            Debug.Log("Found player");
+            ChasePlayer(withinAggroColliders[0].GetComponent<FirstPersonPlayer>());
+        }
+    }
+
+    public void TakeDamage(int _damageAmount)
+    {
+        _enemyHealth -= _damageAmount;
+        Debug.Log("Enemy has taken damage!");
+        Debug.Log(_enemyHealth + " health remaining");
+        if (_enemyHealth <= 0)
+        {
+            _enemyHealth = 0;
+        }
+
+    }
+
+    void ChasePlayer(FirstPersonPlayer player)
+    {
+        this.player = player;
+        navAgent.SetDestination(player.transform.position);
+    }
+}
---
+++ Assets/Scripts/FireWeapon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWeapon : MonoBehaviour
+{
+    [SerializeField] Camera cameraController;
+    [SerializeField] Transform rayOrigin;
+    [SerializeField] float shootDistance = 10f;
+    [SerializeField] GameObject visualFeedbackObject; // hitpoint light
+    [SerializeField] int weaponDamage = 20;
+    [SerializeField] LayerMask hitLayers;
+
+    RaycastHit objectHit;   // store info about our raycast hit
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    // fire the weapon using a raycast
+    void Shoot()
+    {
+        // calculate direction to shoot the ray
+        Vector3 rayDirection = cameraController.transform.forward;
+        // cast a debug ray
+        Debug.DrawRay(rayOrigin.position, rayDirection * shootDistance, Color.blue, 1f);
+        // fire the raycast
+        if(Physics.Raycast(rayOrigin.position, rayDirection, out objectHit, shootDistance, hitLayers))
+        {
+            // Print name of object that was hit
+            Debug.Log("You HIT the " + objectHit.transform.name);
+
+            if (objectHit.transform.tag == "Enemy")
+            {
+                Debug.Log("DEAL DAMAGE");
+                // Visual Feedback
+                visualFeedbackObject.transform.position = objectHit.point;
+                // If enemy is hit, apply damage
+                EnemyShooter enemyShooter = objectHit.transform.gameObject.GetComponent<EnemyShooter>();
+                if (enemyShooter != null)
+                {
+                    enemyShooter.TakeDamage(weaponDamage);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Miss");
+        }
+    }
+}
